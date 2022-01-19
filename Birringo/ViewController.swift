01b9f6fd2723f7e56199650adf7970ab630715d5
pablo_garcia <pablo_garcia_tsapp1ta2021@cev.com>
2022-01-19 17:45:32 +0100

    Birringo 1.0

---
+++ Birringo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Birringo
+//
+//  Created by APPS2T on 18/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
