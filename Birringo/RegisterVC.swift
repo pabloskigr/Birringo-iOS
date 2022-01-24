56647d53a35e26766221da752b9af43bed822b76
Jonace <jonathan_miguel_onrubia_tsapp1ta2021@cev.com>
2022-01-24 18:29:43 +0100

    Auto stash before merge of "develop" and "feature/tutorial"

--- Birringo/RegisterVC.swift
+++ Birringo/RegisterVC.swift
@@ -13,7 +13,14 @@ class RegisterVC: UIViewController {
     @IBOutlet weak var view_registerBox: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+<<<<<<< Updated upstream
         self.navigationItem.hidesBackButton = true
+=======
+<<<<<<< HEAD
+        self.navigationItem.hidesBackButton = true
+=======
+>>>>>>> develop
+>>>>>>> Stashed changes
         setupColors()
         view_registerBox.layer.cornerRadius = 30
         view_registerBox.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
@@ -24,11 +31,20 @@ class RegisterVC: UIViewController {
             self.navigationController?.pushViewController(loginVC, animated: true)
         }
     }
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+>>>>>>> Stashed changes
     @IBAction func registerBtnTapped(_ sender: Any) {
         if let tutorialVC = self.storyboard?.instantiateViewController(identifier: "OnboardingVC") as? OnboardingVC {
             self.navigationController?.pushViewController(tutorialVC, animated: true)
         }
     }
+<<<<<<< Updated upstream
+=======
+=======
+>>>>>>> develop
+>>>>>>> Stashed changes
     private func setupColors(){
         view_registerBox.backgroundColor = UIColor(named: "background")
     }
