9ae9cfd0e24683845f966e2d51c664c2c0d4b0a6
pablo_garcia <pablo_garcia_tsapp1ta2021@cev.com>
2022-01-26 19:20:18 +0100

    Perfil.Error

---
+++ Birringo/Cells/PhotoCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Birringo
+//
+//  Created by APPS2T on 26/1/22.
+//
+
+import UIKit
+
+//class PhotoCollectionViewCell: UICollectionViewCell {
+//    static let identifier = "PhotoCollectionViewCell"
+//
+//    override init(frame: CGRect){
+//
+//    }
+//}
---
+++ Birringo/VC/FavoritosVC.swift
@@ -0,0 +1,31 @@
+//
+//  FavoritosVC.swift
+//  Birringo
+//
+//  Created by APPS2T on 26/1/22.
+//
+
+import UIKit
+//
+//class FavoritosVC: UIViewController,UICollectionViewDelegate, UICollectionViewDataSource{
+//   
+//    
+//   
+//    
+//    
+//    let collectionView = UICollectionView(
+//        frame: .zero,
+//        collectionViewLayout: UICollectionViewFlowLayout()
+//    )
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 6
+//    }
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//           
+//       }
+//    
+//}
